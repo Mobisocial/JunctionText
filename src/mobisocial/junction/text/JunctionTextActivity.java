999c605b0a2fa88aeffb06886c484ff8fe2e9c0f
Ben Dodson <bjdodson@handdog.(none)>
2011-04-11 23:41:37 -0700

    Share page over NFC

--- src/mobisocial/junction/text/JunctionTextActivity.java
+++ src/mobisocial/junction/text/JunctionTextActivity.java
@@ -2,8 +2,12 @@ package mobisocial.junction.text;
 
 import java.net.URI;
 
+import mobisocial.nfc.Nfc;
+import mobisocial.nfc.Nfc.NdefFactory;
+
 import org.json.JSONObject;
 
+import edu.stanford.junction.Junction;
 import edu.stanford.junction.JunctionException;
 import edu.stanford.junction.JunctionMaker;
 import edu.stanford.junction.SwitchboardConfig;
@@ -11,6 +15,7 @@ import edu.stanford.junction.api.activity.JunctionActor;
 import edu.stanford.junction.api.messaging.MessageHeader;
 import edu.stanford.junction.provider.xmpp.XMPPSwitchboardConfig;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
@@ -18,19 +23,41 @@ import android.widget.Toast;
 
 public class JunctionTextActivity extends Activity {
 	private static final String TAG = "junctiontext";
+	private Nfc mNfc;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+        mNfc = new Nfc(this);
+
         joinOrDie();
         buildUi();
     }
     
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	mNfc.onResume(this);
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	mNfc.onPause(this);
+    }
+    
+    @Override
+    protected void onNewIntent(Intent intent) {
+    	if (mNfc.onNewIntent(this, intent)) return;
+    }
+    
     private void joinOrDie() {
         try {
+        	
         	URI invitation = URI.create("junction://prpl.stanford.edu/jxtext");
+        	URI webInvite = URI.create(Junction.toWebInvite("http://prpl.stanford.edu/junction/text", invitation.toString()));
+        	mNfc.share(NdefFactory.fromUri(webInvite));
             SwitchboardConfig config = new XMPPSwitchboardConfig("prpl.stanford.edu");
         	JunctionMaker.getInstance(config).newJunction(invitation, mStatusUpdater);
         	// 	TODO: Junction.from(Uri invitation).bind(mRunner);
